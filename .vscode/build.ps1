3395930b3da8f16a454c7f20fe81acf70afb4824
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-06-17 13:12:25 +0200

    chore: adjust project layout to my standards

---
+++ .vscode/build.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = 'Stop'
+
+$Configuration = 'Debug'
+
+$VersionTargetPrefix = "D:\RimWorld"
+$VersionTargetSuffix = "Mods\TacticsMode"
+$Target = "$VersionTargetPrefix\1.5\$VersionTargetSuffix"
+
+# build dlls
+dotnet build --configuration $Configuration Source/TacticsMode.csproj
+if ($LASTEXITCODE -gt 0) {
+    throw "Build failed"
+}
+
+# remove mod folder
+Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
+
+# copy mod files
+Copy-Item -Path Assemblies $Target\Assemblies -Recurse
+
+Copy-Item -Path Textures $Target\Textures -Recurse
+
+New-Item -Path $Target -ItemType Directory -Name About
+Copy-Item -Path About\About.xml $Target\About
+Copy-Item -Path About\Preview.png $Target\About
+#Copy-Item -Path About\ModIcon.png $Target\About
+Copy-Item -Path About\PublishedFileId.txt $Target\About
